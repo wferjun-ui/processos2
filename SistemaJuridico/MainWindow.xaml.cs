e72a4650ac086998ff4feb207e9c80920e901395
wferjun-ui <wfer.jun@gmail.com>
2026-02-10 13:17:07 -0300

    Create MainWindow.xaml.cs

---
+++ SistemaJuridico/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+
+namespace SistemaJuridico
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
